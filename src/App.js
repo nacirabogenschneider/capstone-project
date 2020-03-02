a185fdb87a6d840f47ce9cd204be171712ac8291
Nacira Bogenschneider <mail@naciradesign.de>
2020-03-02 11:04:00 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+import mapStyles from './components/utils/mapStyles'
+import {
+  withGoogleMap,
+  withScriptjs,
+  GoogleMap,
+  Marker,
+  InfoWindow,
+} from 'react-google-maps'
+import * as schoolsData from './schools.json'
+
+function App() {
+  return
+}
+
+export default App
+
+//import logo from './logo.svg';
+{
+  /* <img src={logo} className="App-logo" alt="logo" /> */
+}
