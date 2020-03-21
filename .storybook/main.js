0a04f45c0adf3286b56241c8695380b6b93ad891
Nacira Bogenschneider <mail@naciradesign.de>
2020-03-21 14:28:16 +0100

    Reinstall npm

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,8 @@
-export const stories = ['../src/**/*.stories.js']
-export const addons = [
-  '@storybook/preset-create-react-app',
-  '@storybook/addon-actions',
-  '@storybook/addon-links',
-  '@storybook/addon-actions/register',
-  '@storybook/addon-knobs/register',
-  '@storybook/addon-notes/register',
-]
+module.exports = {
+  stories: ['../src/**/*.stories.js'],
+  addons: [
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+  ],
+};
